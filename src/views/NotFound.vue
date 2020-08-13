c9c4906a8298c91515d001e2abe49d03370577a0
XXXXXie1997 <1416849251@qq.com>
2020-08-13 15:01:23 +0800

    添加404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>当前页面不存在，请检查链接是否正确</div>
+    <div>
+      <router-link to="/">返回首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
