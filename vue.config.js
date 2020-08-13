798dc8433a65fef69a2530fc1129fd08ae035f8b
XXXXXie1997 <1416849251@qq.com>
2020-08-13 21:12:25 +0800

    普通的更新

--- vue.config.js
+++ vue.config.js
@@ -11,12 +11,12 @@ module.exports = {
       .end()
       .use("svg-sprite-loader")
       .loader("svg-sprite-loader")
-      .options({ extract: false })
+      .options({extract: false})
       .end();
 
     config
       .plugin("svg-sprite")
-      .use(require("svg-sprite-loader/plugin"), [{ plainSprite: true }]);
+      .use(require("svg-sprite-loader/plugin"), [{plainSprite: true}]);
     config.module.rule("svg").exclude.add(dir);
   },
 };
