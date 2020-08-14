2b718120624c1db9aa65d6f6f2478bf7512d132e
XXXXXie1997 <1416849251@qq.com>
2020-08-14 12:35:06 +0800

    模块化（type跑到页面顶部了。。排查中）

---
+++ src/components/Money/Notes.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <label class="notes">
+            <span class="name">备注</span>
+            <input type="text" placeholder="在这里输入备注">
+        </label>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "Notes"
+  }
+</script>
+
+<style lang="scss" scoped>
+    .notes {
+        color: whitesmoke;
+        font-size: 16px;
+        background: rgba(65,171,226,0.8);
+        padding-left: 16px;
+        display: flex;
+        align-items: center;
+
+    .name {
+        padding-right: 16px;
+    }
+
+    input {
+        font-size: 14px;
+        color: whitesmoke;
+        height: 64px;
+        flex-grow: 1;
+        background: transparent;
+        border: none;
+        padding-right: 16px;
+    }
+    }
+
+</style>
---
+++ src/components/Money/Tags.vue
@@ -0,0 +1,61 @@
+<template>
+
+    <div class="tags">
+        <div class="new">
+            <button>新增标签</button>
+        </div>
+        <ul class="current">
+            <li>衣</li>
+            <li>食</li>
+            <li>住</li>
+            <li>行</li>
+        </ul>
+    </div>
+
+</template>
+
+<script>
+  export default {
+    name: "Tags"
+  }
+</script>
+
+<style lang="scss" scoped>
+    .tags {
+        font-size: 14px;
+        padding: 16px;
+        flex-grow: 1;
+        display: flex;
+        flex-direction: column-reverse;
+
+    > .current {
+        display: flex;
+        flex-wrap: wrap;
+
+    > li {
+        background: rgb(61,140,181);
+        color: whitesmoke;
+        $h: 24px;
+        height: $h;
+        line-height: $h;
+        border-radius: $h/2;
+        padding: 0 16px;
+        margin-right: 12px;
+        margin-top: 4px;
+    }
+    }
+
+    > .new {
+        padding-top: 16px;
+
+    button {
+        background: transparent;
+        border: none;
+        color: rgba(61,129,166,0.7);
+        border-bottom: 1px solid rgb(64,161,211);
+        padding: 0 4px;
+    }
+    }
+    }
+
+</style>
