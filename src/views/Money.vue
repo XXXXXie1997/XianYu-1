e1f3febfb396485de762b03d9e1147ccedcdf9dd
XXXXXie1997 <1416849251@qq.com>
2020-08-17 10:59:18 +0800

    添加createdAt字段

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,6 @@
 <template>
     <Layout class-prefix="layout">
-        {{record}}
+        {{recordList}}
         <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
         <Types :value.sync="record.type"/>
         <Notes @update:value='onUpdateNotes'/>
@@ -17,11 +17,25 @@
   import Types from "@/components/Money/Types.vue";
   import {Component, Watch} from "vue-property-decorator";
 
+  const version = window.localStorage.getItem('version') || 0;
+  const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
+  //version0.0.2 数据迁移
+  if (version === '0.0.1') {
+    recordList.forEach(record => {
+      record.createdAt = new Date(2020, 1, 1);
+    });
+    //保存数据
+    window.localStorage.setItem('recordList', JSON.stringify(recordList));
+  }
+  //升级版本
+  window.localStorage.setItem('version', '0.0.2');
+
   type Record = {
     tags: string[];
     notes: string;
     type: string;
     amount: number;
+    createdAt?: Date;
   }
 
   @Component({
@@ -35,8 +49,7 @@
       type: '-',
       amount: 0
     };
-    recordList: Record[] = [];
-
+    recordList: Record[] = recordList;
     onUpdateTags(value: string[]) {
       this.record.tags = value;
     }
@@ -46,9 +59,9 @@
     }
 
     saveRecord() {
-      const record2 = JSON.parse(JSON.stringify(this.record));
+      const record2: Record = JSON.parse(JSON.stringify(this.record));
+      record2.createdAt = new Date();
       this.recordList.push(record2);
-      console.log(this.recordList);
     }
 
     @Watch('recordList')
