087302fe2cae567b105cd06a5ba2fc6338b50b10
XXXXXie1997 <1416849251@qq.com>
2020-08-20 19:49:32 +0800

    完成标签的创建和展示功能

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,10 +1,26 @@
 const localStorageKeyName = 'tagList';
-const tagListModel = {
+type TagListModel = {
+  data: string[];
+  fetch: () => string[];
+  create: (name: string) => 'success' | 'duplicated';  //联合类型
+  save: () => void;
+}
+const tagListModel: TagListModel = {
+  data: [],
   fetch() {
-    return JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+    this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]');
+
+    return this.data;
   },
-  save(data: RecordItem[]) {
-    window.localStorage.setItem(localStorageKeyName, JSON.stringify(data));
+  create(name: string) {
+    if (this.data.indexOf(name) >= 0) {return 'duplicated';}
+    this.data.push(name);
+    this.save();
+    return 'success';
+
+  },
+  save() {
+    window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
   }
 };
 
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,71 +1,79 @@
 <template>
-  <Layout>
-    <ol class="tags">
-      <li>
-        <span>衣</span>
-        <Icon name="right" />
-      </li>
-      <li>
-        <span>食</span>
-        <Icon name="right" />
-      </li>
-      <li>
-        <span>住</span>
-        <Icon name="right" />
-      </li>
-      <li>
-        <span>行</span>
-        <Icon name="right" />
-      </li>
-    </ol>
-    <div class="createTag-wrapper">
-      <button class="createTag">新建标签</button>
-    </div>
-  </Layout>
+    <Layout>
+        <ol class="tags">
+            <li v-for="tag in tags" :key="tag">
+                <span>{{tag}}</span>
+                <Icon name="right"/>
+            </li>
+        </ol>
+        <div class="createTag-wrapper">
+            <button class="createTag" @click="createTag">新建标签</button>
+        </div>
+    </Layout>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue'
-  import {Component} from 'vue-property-decorator'
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+  import tagListModel from "@/models/tagListModel";
+
+
+  tagListModel.fetch();
 
   @Component
-export default class Labels extends Vue{
+  export default class Labels extends Vue {
+    tags = tagListModel.data;
 
-}
-</script>
-
-<style lang="scss" scoped>
-  .tags {
-    color: white;
-    background: rgb(83,178,228);
-    font-size: 16px;
-    padding-left: 16px;
-    > li {
-      min-height: 44px;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      border-bottom: 1px solid rgb(56,107,134);
-      svg {
-        width: 18px;
-        height: 18px;
-        color: #666;
-        margin-right: 16px;
+    createTag() {
+      const name = window.prompt('请输入标签名');
+      if (name) {
+        const message = tagListModel.create(name);
+        if (message === 'duplicated') {
+          window.alert('标签名重复，请检查');
+        } else if (message === 'success') {
+          window.alert('创建成功');
+        }
       }
     }
   }
-  .createTag {
-    background: rgb(59,127,164);
-    color: white;
-    border-radius: 4px;
-    border: none;
-    height: 40px;
-    padding: 0 16px;
-    &-wrapper {
-      text-align: center;
-      padding: 16px;
-      margin-top: 44-16px;
+</script>
+
+<style lang="scss" scoped>
+    .tags {
+        color: white;
+        background: rgb(83, 178, 228);
+        font-size: 16px;
+        padding-left: 16px;
+
+        > li {
+            min-height: 44px;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            border-bottom: 1px solid rgb(56, 107, 134);
+
+            svg {
+                width: 18px;
+                height: 18px;
+                color: #666;
+                margin-right: 16px;
+            }
+        }
     }
-  }
+
+    .createTag {
+        background: rgb(59, 127, 164);
+        color: white;
+        border-radius: 4px;
+        border: none;
+        height: 40px;
+        padding: 0 16px;
+
+        &-wrapper {
+            text-align: center;
+            padding: 16px;
+            margin-top: 44-16px;
+        }
+    }
 
 </style>
