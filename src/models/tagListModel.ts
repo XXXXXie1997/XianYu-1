d4ebfe1a63ca87c7a7c4cfa3d865436f09e828bb
XXXXXie1997 <1416849251@qq.com>
2020-08-20 20:18:00 +0800

    将tag类型由string改为{id,name}

--- src/models/tagListModel.ts
+++ src/models/tagListModel.ts
@@ -1,7 +1,11 @@
 const localStorageKeyName = 'tagList';
+type Tag = {
+  id: string;
+  name: string;
+}
 type TagListModel = {
-  data: string[];
-  fetch: () => string[];
+  data: Tag[];
+  fetch: () => Tag[];
   create: (name: string) => 'success' | 'duplicated';  //联合类型
   save: () => void;
 }
@@ -13,8 +17,9 @@ const tagListModel: TagListModel = {
     return this.data;
   },
   create(name: string) {
-    if (this.data.indexOf(name) >= 0) {return 'duplicated';}
-    this.data.push(name);
+    const names = this.data.map(item=>item.name)
+    if (names.indexOf(name) >= 0) {return 'duplicated';}1
+    this.data.push({id:name,name:name});
     this.save();
     return 'success';
 
