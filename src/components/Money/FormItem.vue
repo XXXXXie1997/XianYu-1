133a8acd007ddd1f26b7031480866b5ef07fb3ce
XXXXXie1997 <1416849251@qq.com>
2020-08-24 11:47:32 +0800

    修改了一些错误

--- src/components/Money/FormItem.vue
+++ src/components/Money/FormItem.vue
@@ -5,7 +5,7 @@
             <input type="text"
                    :value="value"
                    @input="onValueChanged($event.target.value)"
-                   :placeholder="this.placeholder">
+                   :placeholder="placeholder">
         </label>
     </div>
 </template>
@@ -21,7 +21,6 @@
     @Prop({required:true}) fieldName!: string;
     @Prop() placeholder?: string;
 
-    @Watch('value')
     onValueChanged(value: string){
       this.$emit('update:value',value)
     }
