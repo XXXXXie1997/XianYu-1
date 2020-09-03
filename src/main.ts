5a14f0d09f7b186a52bfa50532a0a5385d654f8b
XXXXXie1997 <1416849251@qq.com>
2020-09-03 12:35:32 +0800

    使用图床

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ window.onload = function () {
 if(document.documentElement.clientWidth>500){
   window.alert('使用手机打开本页面已获得最佳体验')
   const img = document.createElement('img')
-  img.src = '/blob/gh-pages/qrcode.png'
+  img.src = 'https://imgchr.com/i/wC6sqP'
   img.style.position = 'fixed'
   img.style.left = '50%'
   img.style.top = '50%'
